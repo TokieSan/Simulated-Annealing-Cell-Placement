b595640ce1cf92766f39917b5e18eef622286479
qedady <amer.esmail48@gmail.com>
2024-05-10 05:00:13 +0200

    Only used vectors and resolved bugs. Now, it is working perfectly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
     
 using namespace std;
 
@@ -12,11 +10,11 @@ int num_cells, num_nets, ny, nx;
 int initial_cost;
 
 int calc_cost(int idx) { // net's index
-    assert(idx < net_to_cell.size() && idx >= 0);
+   // assert(idx < net_to_cell.size() && idx >= 0);
     int dist = 0;
 
     auto &cur_n2c = net_to_cell[idx];
-    assert(cur_n2c.size() > 0);
+    //assert(cur_n2c.size() > 0);
     int maxx = 0, maxy = 0, minx = INT_MAX, miny = INT_MAX;
 
     for (auto &x : cur_n2c) {
@@ -34,15 +32,15 @@ void print_placement(int cost = initial_cost) {
     for (auto &u : grid) {
         for (auto &v : u) {
             //printf("%d ", v);
-            if (~v) cout << fixed << setw(3) << v << ' ';
+            if (v!=-1) cout << fixed << setw(3) << v << ' ';
             else cout << fixed << setw(3) << "--" << ' ';
         }
         cout << '\n';
     }
-    cout << "CUR COST: " << cost << '\n';
+    cout << "COST: " << cost << '\n';
 }
 
-default_random_engine rd{static_cast<long unsigned int>(97)};
+default_random_engine rd{static_cast<long unsigned int>(1)};
 std::mt19937 gen(rd());
 uniform_real_distribution<> dis(0.0, 1.0);
 bool do_i_do(double probability) {
@@ -51,7 +49,6 @@ bool do_i_do(double probability) {
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(0);
     freopen(argv[1], "r", stdin);
-    //freopen("out", "w", stdout);
 
     cin >> num_cells >> num_nets >> ny >> nx;
 
@@ -108,49 +105,54 @@ int main(int argc, char *argv[]) {
         int ctr = num_moves;
         while (ctr--) {
             int U = gen() % num_cells;
+            int Ux = placement[U].first, Uy = placement[U].second;
             int Vx = gen() % ny, Vy = gen() % nx;
             int V = grid[Vx][Vy];
 
-            if (Vx == placement[U].first && Vy == placement[U].second) {
-                ctr++;
-                if (nx == 1 && ny == 1) break;
-                continue;
-            }
-
-            vector<int> to_br;
-            for (auto &u : cell_to_nets[U]) to_br.push_back(u);
-            if (~V) for (auto &u : cell_to_nets[V]) to_br.push_back(u);
-
-            sort(to_br.begin(), to_br.end());
-            to_br.erase(unique(to_br.begin(), to_br.end()), to_br.end());
-
             int new_cost = cur_cost;
             
-            assert(Vx < grid.size() && Vy < grid[0].size());
-            assert(U < placement.size());
+            // assert(Vx < grid.size() && Vy < grid[0].size());
+            // assert(U < placement.size());
             // swap grid cells
-            swap(grid[Vx][Vy], grid[placement[U].first][placement[U].second]);
+            swap(grid[Vx][Vy], grid[Ux][Uy]);
 
             // swap placements if nonempty, if has empty don't swap
             if (V != -1) swap(placement[U], placement[V]);
             else placement[U] = make_pair(Vx, Vy);
 
-
-            vector<int> temp_net_len(to_br.size());
-            for (int i = 0; i < to_br.size(); i++) {
-                new_cost -= net_len[to_br[i]];
-                temp_net_len[i] = calc_cost(to_br[i]);
-                new_cost += temp_net_len[i];
+            vector<int> temp_net_len(num_nets, -1);
+            for (auto &u : cell_to_nets[U]){
+                new_cost -= net_len[u];
+                temp_net_len[u] = calc_cost(u);
+                new_cost += temp_net_len[u];
+            }
+            if (V != -1) {
+                for (auto &v: cell_to_nets[V]) {
+                    if (temp_net_len[v] != -1)
+                        continue;
+                    new_cost -= net_len[v];
+                    temp_net_len[v] = calc_cost(v);
+                    new_cost += temp_net_len[v];
+                }
             }
                 
             double rej_prop = 1 - exp(-(new_cost - cur_cost) / T_cur);
             if (new_cost >= cur_cost && do_i_do(rej_prop) ) {
-                swap(grid[Vx][Vy], grid[placement[U].first][placement[U].second]);
+                // Revert (Swap back)
+                swap(grid[Vx][Vy], grid[Ux][Uy]);
                 if (V != -1) swap(placement[U], placement[V]);
-                else placement[U] = make_pair(Vx, Vy);
+                else placement[U] = make_pair(Ux, Uy);
             } else {
+                // Swap
                 cur_cost = new_cost;
-                for (int i = 0; i < to_br.size(); i++) net_len[to_br[i]] = temp_net_len[i];
+                 for (auto &u : cell_to_nets[U]){
+                    net_len[u] = temp_net_len[u];
+                 }
+                   if (V != -1) {
+                for (auto &v: cell_to_nets[V]) {
+                    net_len[v] = temp_net_len[v];
+                }
+            }
             }
         }
         T_cur *= cooling_rate;
@@ -158,8 +160,9 @@ int main(int argc, char *argv[]) {
     print_placement(cur_cost);
     cout << "T_cur = " << T_cur << "\n----------------------\n";
 
-
-    
+//  for (int i=0; i<num_cells; i++) {
+//         cout << i<<" "<< placement[i].first << " " << placement[i].second << "\n";
+//     }
 
     return 0;
 }
